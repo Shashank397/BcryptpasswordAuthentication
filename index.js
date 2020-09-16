82d3229288491cec27439ad1a82a7345d6dbc4de
Shashank <shashanksoni397@gmail.com>
2020-09-16 16:49:28 +0530

    added password comparision

--- index.js
+++ index.js
@@ -18,11 +18,24 @@ app.post('/signup', async function(req, res){
     res.status(200).send()
 })
 
-// app.post("/login", async (req, res) =>{
-//     if(await autheticate.find){
-
-//     }
-// })
+app.post("/login", async function(req, res){
+    var user = await autheticate.findOne({where: {username : req.body.username}})
+    if(user==null)
+    {
+        res.status(400).send('can not find user')
+    }
+    try{
+        if(await bcrypt.compare(req.body.password, user.password)) 
+        {
+            res.send('Success')
+        } else{
+            res.send('not allowed')
+        }
+    }
+    catch{
+        res.status(500).send('got some error')
+    }
+})
 
 
 app.listen(9000, ()=> console.log("server is running"))
